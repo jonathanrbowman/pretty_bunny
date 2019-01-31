38c76452b16a2bd5f5a58330a63b76a9f7edee35
Jonathan Bowman <jonathanrbowman@me.com>
2019-01-30 21:40:36 -0500

    Init

---
+++ lib/pretty_bunny.rb
@@ -0,0 +1,6 @@
+require "pretty_bunny/version"
+
+module PrettyBunny
+  class Error < StandardError; end
+  class Engine < ::Rails::Engine; end
+end
---
+++ lib/pretty_bunny/version.rb
@@ -0,0 +1,3 @@
+module PrettyBunny
+  VERSION = "0.1.0"
+end
